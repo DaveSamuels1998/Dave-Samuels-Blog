66eec6d2fc7081a4f94ea9ee85d25133aa9f9e31
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-02 17:06:06 -0500

    update

--- .eleventy.js
+++ .eleventy.js
@@ -9,8 +9,23 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+  const markdownIt = require('markdown-it');
+  const markdownItAnchor = require('markdown-it-anchor');
+  
+  module.exports = function (eleventyConfig) {
+    eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
+  };
 
+  const pluginTOC = require('@uncenter/eleventy-plugin-toc');
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(pluginTOC);
+};
   return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
     dir: {
       input: "src",
       output: "public"
