64f6b05094c29a2d673dc732841886786c8520aa
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-02 18:44:13 -0500

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
-const {DateTime } = require("luxon")
+const {DateTime } = require("luxon");
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('@uncenter/eleventy-plugin-toc');
 
 module.exports = function(eleventyConfig) {
-
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
@@ -9,18 +11,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
-  const markdownIt = require('markdown-it');
-  const markdownItAnchor = require('markdown-it-anchor');
   
-  module.exports = function (eleventyConfig) {
-    eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
-  };
+  eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
+  eleventyConfig.addPlugin(pluginTOC);
 
-  const pluginTOC = require('@uncenter/eleventy-plugin-toc');
-
-module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(pluginTOC);
-};
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
@@ -31,4 +25,4 @@ module.exports = function (eleventyConfig) {
       output: "public"
     }
   };
-}
+} 
