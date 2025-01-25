1ef2bed682d3d099a3b630a646cf71c04b953f96
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-25 18:10:54 -0500

    update

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// set variable
+
+// image img variable
+const mainImages = document.querySelectorAll(".images img");
+// modal body variable
+const modalBody = document.querySelector(".modal");
+// modal image variable
+const modalImages = document.querySelector(".modal-img");
+// modal text & close btn variable
+const text = document.querySelector(".modal-text");
+const close = document.querySelector(".closed");
+
+// take ForEach loop in images to appear a classlist on their
+
+mainImages.forEach((image) => {
+ image.addEventListener('click', () => {
+  modalBody.classList.add("active");
+
+  // Individual Title and Individual photos
+
+  modalImages.src = image.src;
+  text.innerHTML = image.alt; 
+  
+  close.addEventListener('click', () => {
+    modalBody.classList.remove("active");
+  });
+ });
+});
+
