f8827328129f147782773ed53899937ed761b141
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-25 20:10:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var modal = document.getElementById('myModal');
 // to all images -- note I'm using a class!
-var images = document.getElementsByClassName('main-article img:not(figure *)');
+var images = document.getElementsByClassName('main-article img');
 // the image in the modal
 var modalImg = document.getElementById("img01");
 // and the caption in the modal
