9ce035f04ba1545bccc3f5adbd459ebd40704569
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-25 19:59:28 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
-
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
-
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('main-article img:not(figure *)');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementsByClassName("text");
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    // modalImg.src = this.src;
+    modalImg.innerHTML = this.innerHTML;
+    captionText.innerHTML = this.alt;
+  }
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
