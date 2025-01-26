ac882ea8086076a944ba70041b97fe75755f1112
DaveSamuels1998 <davesamuels1998@gmail.com>
2025-01-26 09:55:05 -0500

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const {DateTime } = require("luxon");
 const markdownIt = require('markdown-it');
+const markdownItAttrs = require("markdown-it-attrs");
 const markdownItAnchor = require('markdown-it-anchor');
 const pluginTOC = require('@uncenter/eleventy-plugin-toc');
 
@@ -7,7 +8,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
-
+  // Set up Markdown parser with markdown-it-attrs
+  let markdownLibrary = markdownIt().use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLibrary);
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
